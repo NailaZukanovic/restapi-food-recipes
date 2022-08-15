f2cfa8a155d524bc169f4378dacdad91103bcd65
Naila Zukanovic <86067688+NailaZukanovic@users.noreply.github.com>
2022-08-15 18:08:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const recipeRoutes = require('./api/routes/recipes');
 const categoryRoutes = require('./api/routes/category');
 const userRoutes = require('./api/routes/user');
 
-const MONGODB_URI = `mongodb+srv://Muhamed:test123@cluster0-nulcu.mongodb.net/foodrecipes`
+const MONGODB_URI = 'mongodb+srv://mongo:yKI4hF1bRag7TGLq@cluster0.tb2zz.mongodb.net/test'
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
